664b12bfb5ce93f4a8fc4cb008be0f11a182d956
wisehead <alex.chenhui@gmail.com>
2022-03-08 17:15:40 +0800

    File

---
+++ basic/15.File/chgdir.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+import os
+ 
+# 将当前目录改为"/home/newdir"
+os.chdir("/home/newdir")
---
+++ basic/15.File/rmdir.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+import os
+ 
+# 删除”/tmp/test”目录
+os.rmdir( "/tmp/test"  )
