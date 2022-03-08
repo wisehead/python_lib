2f490ff66a6067987588a9c2a387bae71dc65aaa
wisehead <alex.chenhui@gmail.com>
2022-03-08 15:58:03 +0800

    utf8

---
+++ basic/02.UTF8/utf8-vim.py
@@ -0,0 +1,9 @@
+#############################################################
+#   File Name: utf8.py
+#     Autohor: Hui Chen (c) 2022
+#        Mail: chenhui13@xxx.com
+# Create Time: 2022/03/08-15:45:13
+#############################################################
+#!/usr/bin/python
+#vim: set fileencoding:utf-8
+print( "你好" )
