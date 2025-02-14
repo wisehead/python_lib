45efb25e4b262f42350900b45245b658d367493a
Hui Chen <alex.chenhui@gmail.com>
2025-02-14 08:57:37 +0800

    dict

---
+++ 04.python3/05.comprehension/dict.py
@@ -0,0 +1,4 @@
+listdemo = ['Google','Runoob', 'Taobao']
+# 将列表中各字符串值为键，各字符串的长度为值，组成键值对
+newdict = {key:len(key) for key in listdemo}
+print(newdict)
---
+++ 04.python3/05.comprehension/dict2.py
@@ -0,0 +1,3 @@
+dic = {x: x**2 for x in (2, 4, 6)}
+print(dic)
+print(type(dic))
